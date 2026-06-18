71cd2d3f9246fd1fe7019594e2c95ca2c6837292
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-06-18 09:51:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request
 #biblioteca para conectar ao banco de dados MySQL
 import mysql.connector
 
+app = Flask(__name__)
+
 #Cria conexão com o banco de dados
 bd_config = {
     'host': '127.0.0.1',
@@ -14,3 +16,31 @@ bd_config = {
     'database': 'cadastro1'
 }
 
+#Criação de rota para o arquivo HTML principal
+
+@app.route('/')
+
+def indexRota():
+    return render_template('index.html')
+
+@app.route('/cadastrar', methods=('POST'))
+
+def criarCadastro():
+    #recebe os dados do formulário
+    cpf = request.form['cpf']
+    primeiro_nome = request.form['primeiro_nome']
+    sobrenome = request.form['sobrenome']
+    idade = request.form['idade']
+
+    #Conecta ao banco de dados
+    conexao = mysql.connector.connect(**bd_config)
+
+    #levar instrução SQL do python para o banco de dados
+    curso = conexao.cursor()
+
+    query = "INSERT INTO tb_cliente (cpf, primeiro_nome, sobrenome, idade) VALUES (%s, %s, %s, %s)"
+    
+
+
+#Biblioteca mysql.connector conecta o python com o mysql
+#Decorador tem @
